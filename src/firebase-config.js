e1d6d747a7c73b1021041f1883bdcee298ea1b04
blackbin98 <blackbin98@pukyong.ac.kr>
2026-06-09 04:27:23 +0900

    Add files via upload

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_vwVcNhkpQPUPA2pvywsc1RAVIPykRuw",
+  authDomain: "holzzak-8bcf7.firebaseapp.com",
+  databaseURL: "https://holzzak-8bcf7-default-rtdb.firebaseio.com",
+  projectId: "holzzak-8bcf7",
+  storageBucket: "holzzak-8bcf7.firebasestorage.app",
+  messagingSenderId: "122240882256",
+  appId: "1:122240882256:web:868893a18574f3898d2355",
+  measurementId: "G-E5K65JS217"
+};
